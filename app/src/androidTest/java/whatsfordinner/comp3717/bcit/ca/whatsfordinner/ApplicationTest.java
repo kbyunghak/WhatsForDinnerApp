640b884d4cad3737cf7a141378a545f6fb71b6a8
kbyunghak <kbyunghak@gmail.com>
2015-11-13 15:23:28 -0800

    initial commit

---
+++ app/src/androidTest/java/whatsfordinner/comp3717/bcit/ca/whatsfordinner/ApplicationTest.java
@@ -0,0 +1,13 @@
+package whatsfordinner.comp3717.bcit.ca.whatsfordinner;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
