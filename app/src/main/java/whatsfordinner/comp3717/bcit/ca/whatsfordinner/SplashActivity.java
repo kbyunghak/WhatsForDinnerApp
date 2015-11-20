7cff5709a3066098740ba9ae52fb3f07dfdae400
Mae Yee <mae.cast@hotmail.com>
2015-11-20 01:55:06 -0800

    Set timer for Splash page

--- app/src/main/java/whatsfordinner/comp3717/bcit/ca/whatsfordinner/SplashActivity.java
+++ app/src/main/java/whatsfordinner/comp3717/bcit/ca/whatsfordinner/SplashActivity.java
@@ -2,10 +2,10 @@ package whatsfordinner.comp3717.bcit.ca.whatsfordinner;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -13,6 +13,18 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        // set a timer for the splash page
+        // after 3 seconds, load the MainActivity
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 
     @Override
@@ -37,8 +49,13 @@ public class SplashActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+// onClick handler for navigating to MainActivity
+// not needed if timer is used
+/*
     public void loadMainPage(final View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+*/
+
 }
